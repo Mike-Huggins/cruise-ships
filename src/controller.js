4fff7f06c998e472172e0ffeae3fd97ea80a99a3
Mike H <mikeyh1001@hotmail.com>
2019-03-31 13:39:09 +0100

    the sea moves...

--- src/controller.js
+++ src/controller.js
@@ -3,4 +3,10 @@ function Controller() {
 }
 
 Controller.prototype.initialiseSea = function initialiseSea() {
+  const backgrounds = ['./images/water0.png', './images/water1.png'];
+  let backgroundIndex = 0;
+  window.setInterval(() => {
+    document.querySelector('#viewport').style.backgroundImage = `url('${backgrounds[backgroundIndex % backgrounds.length]}')`;
+    backgroundIndex += 1;
+  }, 1000);
 };
